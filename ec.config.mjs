401f416a26f342ec86ea217db2813447f47f7474
Sandrine Boissel <sandrineboisse@gmail.com>
2026-08-10 18:40:41 +0200

    Remove template artifacts and personalize portfolio repo

--- ec.config.mjs
+++ ec.config.mjs
@@ -20,7 +20,7 @@ const isDarkCodeTheme = ({ theme }) => theme.name === code.darkTheme;
 
 export default defineEcConfig({
   plugins: [pluginLineNumbers(), pluginCollapsibleSections()],
-  defaultLocale: 'zh-CN',
+  defaultLocale: 'en',
   themes: [code.lightTheme, code.darkTheme],
   themeCssRoot: ':root',
   themeCssSelector: (theme) =>
